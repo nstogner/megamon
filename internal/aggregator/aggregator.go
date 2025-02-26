4dbe34c94ae5258ae0664ba7f4edc26bce600b72
Edwin Chiu <edwinchiu@google.com>
2025-02-26 12:58:36 -0500

    add tpu chip count to nodePoolsUp and nodePoolsUpSummaries

--- internal/aggregator/aggregator.go
+++ internal/aggregator/aggregator.go
@@ -151,6 +151,11 @@ func (a *Aggregator) Aggregate(ctx context.Context) error {
 				return
 			}
 			up.ExpectedCount = expectedCount
+			if tpuChipCount, err := k8sutils.TpuTopologyToChipCount(up.TPUTopology); err != nil {
+				log.Printf("WARNING: failed to convert TPU topology to chip count for node pool %q: %v", np.Name, err)
+			} else {
+				up.TPUChipCount = int32(tpuChipCount)
+			}
 			report.NodePoolsUp[np.Name] = up
 		}()
 	}
