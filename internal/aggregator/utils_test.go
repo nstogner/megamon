a2fa4ab2bb68261d80f420bf3eda45ea8361821a
Edwin Chiu <edwinchiu@google.com>
2025-03-05 14:46:33 -0500

    unit tests for extractNodePoolAttrs and extractJobSetAttrs

--- internal/aggregator/utils_test.go
+++ internal/aggregator/utils_test.go
@@ -3,8 +3,16 @@ package aggregator
 import (
 	"testing"
 
+	"example.com/megamon/internal/k8sutils"
+	"example.com/megamon/internal/records"
+	"github.com/google/go-cmp/cmp"
 	"github.com/stretchr/testify/require"
+
 	containerv1beta1 "google.golang.org/api/container/v1beta1"
+	batchv1 "k8s.io/api/batch/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	jobset "sigs.k8s.io/jobset/api/jobset/v1alpha2"
 )
 
 func TestGetExpectedTPUNodePoolSize(t *testing.T) {
@@ -71,6 +79,202 @@ func TestGetExpectedTPUNodePoolSize(t *testing.T) {
 				require.ErrorContains(t, err, c.wantErrContains)
 			}
 			require.Equal(t, c.want, got)
+		})
+	}
+}
+
+func TestExtractJobSetAttrs(t *testing.T) {
+	cases := map[string]struct {
+		js   *jobset.JobSet
+		want records.Attrs
+	}{
+		"empty": {
+			js:   &jobset.JobSet{},
+			want: records.Attrs{},
+		},
+		"basic": {
+			js: &jobset.JobSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-jobset",
+					Namespace: "test-ns",
+					UID:       "12345",
+				},
+				Spec: jobset.JobSetSpec{
+					ReplicatedJobs: []jobset.ReplicatedJob{
+						{
+							Replicas: 1,
+							Template: batchv1.JobTemplateSpec{
+								Spec: batchv1.JobSpec{
+									Template: corev1.PodTemplateSpec{
+										Spec: corev1.PodSpec{
+											NodeSelector: map[string]string{
+												k8sutils.NodeLabelGKETPUAccelerator: "tpu-v5p",
+												k8sutils.NodeLabelGKETPUTopology:    "2x2x1",
+												k8sutils.NodeLabelGKESpot:           "true",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: records.Attrs{
+				JobSetName:      "test-jobset",
+				JobSetNamespace: "test-ns",
+				JobSetUID:       "12345",
+				TPUAccelerator:  "tpu-v5p",
+				TPUTopology:     "2x2x1",
+				Spot:            true,
+				TPUChipCount:    4,
+			},
+		},
+		"multiple replicated jobs": {
+			js: &jobset.JobSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-jobset",
+					Namespace: "test-ns",
+					UID:       "12345",
+				},
+				Spec: jobset.JobSetSpec{
+					ReplicatedJobs: []jobset.ReplicatedJob{
+						{
+							Replicas: 1,
+							Template: batchv1.JobTemplateSpec{
+								Spec: batchv1.JobSpec{
+									Template: corev1.PodTemplateSpec{
+										Spec: corev1.PodSpec{
+											NodeSelector: map[string]string{
+												k8sutils.NodeLabelGKETPUAccelerator: "tpu-v5p",
+												k8sutils.NodeLabelGKETPUTopology:    "2x2x1",
+												k8sutils.NodeLabelGKESpot:           "false",
+											},
+										},
+									},
+								},
+							},
+						},
+						{
+							Replicas: 2,
+							Template: batchv1.JobTemplateSpec{
+								Spec: batchv1.JobSpec{
+									Template: corev1.PodTemplateSpec{
+										Spec: corev1.PodSpec{
+											NodeSelector: map[string]string{
+												k8sutils.NodeLabelGKETPUAccelerator: "tpu-v5p",
+												k8sutils.NodeLabelGKETPUTopology:    "2x4x1",
+												k8sutils.NodeLabelGKESpot:           "false",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: records.Attrs{
+				JobSetName:      "test-jobset",
+				JobSetNamespace: "test-ns",
+				JobSetUID:       "12345",
+				TPUAccelerator:  "tpu-v5p",
+				TPUTopology:     "2x4x1",
+				Spot:            false,
+				TPUChipCount:    20,
+			},
+		},
+		"missing topology": {
+			js: &jobset.JobSet{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      "test-jobset",
+					Namespace: "test-ns",
+					UID:       "12345",
+				},
+				Spec: jobset.JobSetSpec{
+					ReplicatedJobs: []jobset.ReplicatedJob{
+						{
+							Replicas: 1,
+							Template: batchv1.JobTemplateSpec{
+								Spec: batchv1.JobSpec{
+									Template: corev1.PodTemplateSpec{
+										Spec: corev1.PodSpec{
+											NodeSelector: map[string]string{
+												k8sutils.NodeLabelGKETPUAccelerator: "tpu-v5p",
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			want: records.Attrs{
+				JobSetName:      "test-jobset",
+				JobSetNamespace: "test-ns",
+				JobSetUID:       "12345",
+				TPUAccelerator:  "tpu-v5p",
+				Spot:            false,
+				TPUChipCount:    0,
+			},
+		},
+	}
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			got := extractJobSetAttrs(c.js)
+			if diff := cmp.Diff(c.want, got); diff != "" {
+				t.Errorf("extractJobSetAttrs() mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestExtractNodeAttrs(t *testing.T) {
+	cases := map[string]struct {
+		node *corev1.Node
+		want records.Attrs
+	}{
+		"empty": {
+			node: &corev1.Node{},
+			want: records.Attrs{},
+		},
+		"basic": {
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						k8sutils.NodeLabelGKETPUAccelerator: "tpu-v5p",
+						k8sutils.NodeLabelGKETPUTopology:    "2x2x1",
+						k8sutils.NodeLabelGKESpot:           "true",
+					},
+				},
+			},
+			want: records.Attrs{
+				TPUAccelerator: "tpu-v5p",
+				TPUTopology:    "2x2x1",
+				Spot:           true,
+			},
+		},
+		"missing optional fields": {
+			node: &corev1.Node{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: map[string]string{
+						k8sutils.NodeLabelGKETPUAccelerator: "tpu-v5p",
+					},
+				},
+			},
+			want: records.Attrs{
+				TPUAccelerator: "tpu-v5p",
+				Spot:           false,
+			},
+		},
+	}
+	for name, c := range cases {
+		t.Run(name, func(t *testing.T) {
+			got := extractNodeAttrs(c.node)
+			if diff := cmp.Diff(c.want, got); diff != "" {
+				t.Errorf("extractNodeAttrs() mismatch (-want +got):\n%s", diff)
+			}
 		})
 	}
 }
